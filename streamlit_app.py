a4d9956affaf6a7c8eda4675e3f11850b6d7675f
shwetajadhav22620 <80784490+shwetajadhav22620@users.noreply.github.com>
2023-07-21 11:20:50 +0530

    Update streamlit_app.py
    Data should look nice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json()) # jut writes the data to screen
 
-
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
 
